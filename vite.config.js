404e73590f2c2748f564cf6bebbbb8fe04a34248
AkiraKs <adrien.lauriere@etu.unilim.fr>
2024-12-12 16:35:37 +0100

    coolcool

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://akiraks.github.io/santasecret/"
+  base: "/secret-santa-main/", // add repository name here
   plugins: [react()],
 });
